b608cd97f77a5b1b7792703fb04d3259027273ef
Gustavo Coelho <gustavo.coelho@fbter.org.br>
2024-03-11 10:01:57 -0300

    Update AcousticWave2D to enable execution of both the Born and the forward  operator.

--- pylops/waveeqprocessing/twoway.py
+++ pylops/waveeqprocessing/twoway.py
@@ -88,6 +88,7 @@ class AcousticWave2D(LinearOperator):
         checkpointing: bool = False,
         dtype: DTypeLike = "float32",
         name: str = "A",
+        op_name: str = "born",
     ) -> None:
         if devito_message is not None:
             raise NotImplementedError(devito_message)
@@ -104,7 +105,7 @@ class AcousticWave2D(LinearOperator):
             explicit=False,
             name=name,
         )
-
+        self._register_multiplications(op_name)
     @staticmethod
     def _crop_model(m: NDArray, nbl: int) -> NDArray:
         """Remove absorbing boundaries from model"""
@@ -440,12 +441,23 @@ class AcousticWave2D(LinearOperator):
         dtot = np.array(dtot).reshape(nsrc, d.shape[0], d.shape[1])
         return dtot
 
+    def _adj_allshots(self, v: NDArray) -> NDArray:
+        raise Exception("Method not yet implemented")
+
+    def _register_multiplications(self, op_name: str) -> None:
+        if op_name == "born":
+            self._acoustic_matvec = self._born_allshots
+            self._acoustic_rmatvec = self._bornadj_allshots
+        if op_name == "fwd":
+            self._acoustic_matvec = self._fwd_allshots
+            self._acoustic_rmatvec = self._adj_allshots
+
     @reshaped
     def _matvec(self, x: NDArray) -> NDArray:
-        y = self._born_allshots(x)
+        y = self._acoustic_matvec(x)
         return y
 
     @reshaped
     def _rmatvec(self, x: NDArray) -> NDArray:
-        y = self._bornadj_allshots(x)
+        y = self._acoustic_rmatvec(x)
         return y
