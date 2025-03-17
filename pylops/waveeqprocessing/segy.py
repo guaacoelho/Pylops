1bee1887a077a940cb33ee0ef2a1e31dad7f9c5b
Gustavo Coelho <gustavo.coelho@fbter.org.br>
2025-03-17 15:26:05 -0300

    Add segyReader that returns elements based on a segy file

---
+++ pylops/waveeqprocessing/segy.py
@@ -0,0 +1,195 @@
+import segyio
+
+import numpy as np
+from pylops.basicoperators.vstack import VStack
+from pylops.waveeqprocessing.twoway import AcousticWave2D
+# from pylops.waveeqprocessing.twoway import  AcousticWave2D
+
+
+__all__ = ['ReadSEGY2D']
+
+
+class ReadSEGY2D():
+
+    def __init__(self, segy_path):
+
+        self.segyfile = segy_path
+        self.table = self.make_lookup_table(segy_path)
+        self.isRecVariable = self._isRecVariable()
+        self.nsrc = len(self.table)
+
+    def _isRecVariable(self):
+        """
+        Verify if the number of receivers per shots is Regular.
+
+        return True if all shots has the same number of receivers, and False otherwise
+        """
+        # get a set of values representing the number of traces per shot. In other words, the number os receivers per shot
+        n_traces_per_shots = set(v["Num_Traces"] for v in self.table.values())
+
+        # If the lenght of the set is 1, means that all the shots has the same number os receivers
+        return len(n_traces_per_shots) != 1
+
+    def make_lookup_table(self, sgy_file):
+        '''
+        Make a lookup of shots, where the keys are the shot record IDs being
+        searched (looked up)
+
+        Made by Oscar Mojica
+        '''
+        lookup_table = {}
+        with segyio.open(sgy_file, ignore_geometry=True) as f:
+            index = None
+            pos_in_file = 0
+            for header in f.header:
+                if int(header[segyio.TraceField.SourceGroupScalar]) < 0:
+                    scalco = abs(1. / header[segyio.TraceField.SourceGroupScalar])
+                else:
+                    scalco = header[segyio.TraceField.SourceGroupScalar]
+                if int(header[segyio.TraceField.ElevationScalar]) < 0:
+                    scalel = abs(1. / header[segyio.TraceField.ElevationScalar])
+                else:
+                    scalel = header[segyio.TraceField.ElevationScalar]
+                # Check to see if we're in a new shot
+                index = header[segyio.TraceField.FieldRecord]
+                if index not in lookup_table.keys():
+                    lookup_table[index] = {}
+                    lookup_table[index]['filename'] = sgy_file
+                    lookup_table[index]['Trace_Position'] = pos_in_file
+                    lookup_table[index]['Num_Traces'] = 1
+                    lookup_table[index]['Source'] = (header[segyio.TraceField.SourceX] * scalco, header[segyio.TraceField.SourceY] * scalel)
+                    lookup_table[index]['Receivers'] = []
+                else:  # Not in a new shot, so increase the number of traces in the shot by 1
+                    lookup_table[index]['Num_Traces'] += 1
+                lookup_table[index]['Receivers'].append((header[segyio.TraceField.GroupX] * scalco, header[segyio.TraceField.GroupY] * scalel))
+                pos_in_file += 1
+
+        return lookup_table
+
+    def getVelocityModel(self, path):
+        # Read velocity model
+        f = segyio.open(path, iline=segyio.tracefield.TraceField.FieldRecord,
+                        xline=segyio.tracefield.TraceField.CDP)
+        xl, il, t = f.xlines, f.ilines, f.samples
+
+        if len(il) != 1:
+            dims = (len(xl), len(il), len(t))
+        else:
+            dims = (len(xl), len(t))
+
+        vp = f.trace.raw[:].reshape(dims)
+        return vp
+
+    def getSourceCoords(self, index=0):
+        src_coords = np.array(self.table[index]['Source'])
+        sx = np.array([src_coords[0]])
+        sz = np.array([src_coords[-1]])
+
+        return sx, sz
+
+    def getReceiverCoords(self, index=0):
+        recs_coords = np.array(self.table[index]['Receivers'])
+        rx = np.array([coord[0] for coord in recs_coords])
+        rz = np.array([coord[-1] for coord in recs_coords])
+
+        return rx, rz
+
+    def getCoords(self, index=0):
+        rec_coords = self.getReceiverCoords(index)
+        src_coords = self.getSourceCoords(index)
+
+        return src_coords, rec_coords
+
+    def getTn(self):
+        # f = segyio.open(self.segyfile, ignore_geometry=True)
+        with segyio.open(self.segyfile, "r", ignore_geometry=True) as f:
+            num_samples = len(f.samples)
+            samp_int = f.bin[segyio.BinField.Interval] / 1000
+
+        return (num_samples - 1) * samp_int
+
+    def getDt(self):
+        with segyio.open(self.segyfile, "r", ignore_geometry=True) as f:
+            dt = f.bin[segyio.BinField.Interval]  # microseconds
+        return dt / 1000  # return in miliseconds
+
+    def getData(self, index=1, chunk=1):
+        """
+        Return the data from a specific index. The data returned is a 1d array to
+        match the output from VStack
+
+        Parameters
+        ----------
+        index : :obj:`int`
+            Index of the first shot of the chunck
+        chunk : :obj:`int`
+            Size of the chunk. Delimits how many shots will have data returned
+        """
+        with segyio.open(self.segyfile, "r", ignore_geometry=True) as f:
+            retrieved_shot = []
+
+            for nshot in range(index, index + chunk):
+                position = self.table[nshot]['Trace_Position']
+                traces_in_shot = self.table[nshot]['Num_Traces']
+                shot_traces = f.trace[position:position + traces_in_shot]
+
+                for trace in shot_traces:
+                    retrieved_shot.append(trace)
+        # concatena todos os dados em um np.array 1D
+        return np.concatenate(retrieved_shot)
+
+    def _getOperatorChunk(self, shape, origin, spacing, vp0, nbl, space_order, t0, dt, tn, src_type, f0, dtype, chunk, src0_idx, nshots):
+        nsrc = nshots if nshots else self.nsrc
+
+        # Ajusta o limite superior para não ultrapassar o número total de fontes
+        end_idx = min(src0_idx + nsrc, self.nsrc)
+
+        Aops = []
+        for isrc in range(src0_idx, end_idx, chunk):
+            ops = []
+            for i in range(chunk):
+                index = isrc + i
+                # permite que a quantidade de operadores de VSTACK não seja necessáriamento igual
+                if index >= nsrc + src0_idx:
+                    break
+                sx, sz = self.getSourceCoords(index)
+                rx, rz = self.getReceiverCoords(index)
+                Aop = AcousticWave2D(shape=shape, origin=origin, spacing=spacing, vp=vp0, nbl=nbl, space_order=space_order,
+                                     src_x=sx, src_z=sz, rec_x=rx, rec_z=rz, t0=t0, tn=tn, src_type=src_type, f0=f0, dtype=dtype,
+                                     dt=dt)
+                ops.append(Aop)
+            Aops.append(VStack(ops))
+        return Aops
+
+    def _getOperatorUnique(self, shape, origin, spacing, vp0, nbl, space_order, t0, dt, tn, src_type, f0, dtype, src0_idx, nshots):
+        nsrc = nshots if nshots else self.nsrc
+
+        # Ajusta o limite superior para não ultrapassar o número total de fontes
+        end_idx = min(src0_idx + nsrc, self.nsrc)
+
+        Aops = []
+        for isrc in range(src0_idx, end_idx):
+            sx, sz = self.getSourceCoords(isrc)
+            rx, rz = self.getReceiverCoords(isrc)
+            Aop = AcousticWave2D(shape=shape, origin=origin, spacing=spacing, vp=vp0, nbl=nbl, space_order=space_order,
+                                 src_x=sx, src_z=sz, rec_x=rx, rec_z=rz, t0=t0, tn=tn, src_type=src_type, f0=f0, dtype=dtype,
+                                 dt=dt)
+            Aops.append(Aop)
+        return VStack(Aops)
+
+    def getOperator(self, shape, origin, spacing, vp0, nbl, space_order, t0, src_type, f0, dtype, chunk=1, src0_idx=1, nshots=None):
+        """
+        The idea is that each operator is directly related to a shot present in the SEGY file, so we will not have
+        problems with the dimensions of the output data. The basic idea would be a VStack containing
+        nshots operators. However, the size of the data can exceed the computer's memory capacity. To work around
+        this problem, it was defined that a list of VStacks would be created where each VStack would contain a specific
+        number of operators (chunk).
+
+        Thus, chunk defines how many shots/AcousticWave2D operators each VStack will encompass.
+        """
+        dt = self.getDt()
+        tn = self.getTn()
+
+        if chunk == 1:
+            return self._getOperatorUnique(shape, origin, spacing, vp0, nbl, space_order, t0, dt, tn, src_type, f0, dtype, src0_idx, nshots)
+        return self._getOperatorChunk(shape, origin, spacing, vp0, nbl, space_order, t0, dt, tn, src_type, f0, dtype, chunk, src0_idx, nshots)
---
+++ pytests/test_segy.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pytest
+
+from pylops.waveeqprocessing.twoway import AcousticWave2D
+from pylops.waveeqprocessing.segy import ReadSEGY2D
+
+
+def get_parameters():
+    nx = 170
+    nz = 80
+
+    spacing = (25., 25.)  # Grid spacing in m. The domain size is now 1km by 1km
+    origin = (0., 0.)
+    shape = (nx, nz)
+    space_order = 4
+    nbl = 40
+
+    dtype = np.float32
+
+    src_type = "Ricker"
+
+    t0 = 0.
+    f0 = 2  # 0.002 KHz
+
+    vp_top = 1.5
+    vp_bottom = 3.5
+
+    nlayers = 3
+
+    # define a velocity profile in km/s
+    v = np.empty(shape, dtype=dtype)
+    v[:] = vp_top  # Top velocity (background)
+    vp_i = np.linspace(vp_top, vp_bottom, nlayers)
+    for i in range(1, nlayers):
+        v[..., i * int(shape[-1] / nlayers):] = vp_i[i]  # Bottom velocity
+
+    return shape, spacing, origin, space_order, nbl, dtype, src_type, t0, f0, v
+
+
+def create_operator_from_segy(segyReader, shot_id, shape, origin, spacing, v, nbl, space_order, t0, src_type, f0, dtype, tn, dt):
+    """Cria uma instância de AcousticWave2D com base nos dados do SEGY."""
+    sx, sz = segyReader.getSourceCoords(shot_id)
+    rx, rz = segyReader.getReceiverCoords(shot_id)
+
+    return AcousticWave2D(
+        shape=shape, origin=origin, spacing=spacing, vp=v, nbl=nbl,
+        space_order=space_order, src_x=sx, src_z=sz, rec_x=rx, rec_z=rz,
+        t0=t0, tn=tn, src_type=src_type, f0=f0, dtype=dtype, dt=dt
+    )
+
+
+def test_getData_concatenation():
+    path_segy = "/home/gustavo.coelho/segy-files/ModelShots/Anisotropic_FD_Model_Shots_part1.sgy"
+    first_index = 1  # index do primeiro tiro do arquivo segy. A depender do arquivo pode variar começando em 0 ou em 1.
+    segyReader = ReadSEGY2D(path_segy)
+
+    # Retrieve data for multiple shots
+    data1 = segyReader.getData(index=0 + first_index, chunk=1)
+    data2 = segyReader.getData(index=1 + first_index, chunk=1)
+    data3 = segyReader.getData(index=2 + first_index, chunk=1)
+
+    # Retrieve concatenated data
+    concatenated_data = segyReader.getData(index=0 + first_index, chunk=3)
+
+    # Check if concatenation is as expected
+    expected_concatenation = np.concatenate([data1, data2, data3])
+    assert np.array_equal(concatenated_data, expected_concatenation), "Concatenation of data is not as expected"
+
+
+@pytest.mark.parametrize("chunk_size, nshots", [(3, 3), (3, 6), (5, 19), (10, None)])
+def test_getOperator(chunk_size, nshots):
+    path_segy = "/home/gustavo.coelho/segy-files/ModelShots/Anisotropic_FD_Model_Shots_part1.sgy"
+    first_index = 1  # index do primeiro tiro do arquivo segy. A depender do arquivo pode variar começando em 0 ou em 1.
+    segyReader = ReadSEGY2D(path_segy)
+
+    # Obtém parâmetros do modelo
+    shape, spacing, origin, space_order, nbl, dtype, src_type, t0, f0, v = get_parameters()
+
+    # Criação dos operadores a partir do SEGY
+    operator_params = {
+        "shape": shape, "origin": origin, "spacing": spacing, "vp0": v,
+        "nbl": nbl, "space_order": space_order, "t0": t0, "src_type": src_type,
+        "f0": f0, "dtype": dtype, "chunk": chunk_size, "nshots": nshots, "src0_idx": first_index
+    }
+    Aops = segyReader.getOperator(**operator_params)
+
+    dt = segyReader.getDt()
+    tn = segyReader.getTn()
+
+    end_idx = nshots + first_index if nshots else segyReader.nsrc
+    # Criação manual dos operadores de referência
+    reference_operators = [create_operator_from_segy(segyReader, shot_id, shape, origin, spacing, v, nbl, space_order, t0, src_type, f0, dtype, tn, dt)
+                           for shot_id in range(0 + first_index, end_idx)]
+
+    # Teste de consistência entre os operadores retornados e os operadores esperados
+    for i, Aop in enumerate(Aops):
+        for j in range(chunk_size):
+            if i * chunk_size + j > len(Aop.ops):
+                break
+            assert np.array_equal(Aop.ops[j].geometry.rec_positions, reference_operators[i * chunk_size + j].geometry.rec_positions), f"Falha no teste do operador {i}, shot {j}"
