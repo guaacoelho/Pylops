1cd421d499d834c70ac5121d34f0d05cfd9433ff
Gustavo Coelho <gustavo.coelho@fbter.org.br>
2025-04-10 09:16:05 -0300

    Update getData to return data with a specific shape, not longer 1D.
    Add a method to check whether the number of receivers is variable for each shot, and a method to store the ID of the source being executed in that iteration

--- pylops/waveeqprocessing/segy.py
+++ pylops/waveeqprocessing/segy.py
@@ -15,7 +15,23 @@ class ReadSEGY2D():
 
         self.segyfile = segy_path
         self.table = self.make_lookup_table(segy_path)
+        self.isRecVariable = self._isRecVariable()
         self.nsrc = len(self.table)
+
+    def set_shotID(self, id_src):
+        self.id_src = id_src
+
+    def _isRecVariable(self):
+        """
+        Verify if the number of receivers per shots is Regular.
+
+        return True if all shots has the same number of receivers, and False otherwise
+        """
+        # get a set of values representing the number of traces per shot. In other words, the number os receivers per shot
+        n_traces_per_shots = set(v["Num_Traces"] for v in self.table.values())
+
+        # If the lenght of the set is 1, means that all the shots has the same number os receivers
+        return len(n_traces_per_shots) != 1
 
     def make_lookup_table(self, sgy_file):
         '''
@@ -100,130 +116,21 @@ class ReadSEGY2D():
             dt = f.bin[segyio.BinField.Interval]  # microseconds
         return dt / 1000  # return in miliseconds
 
-    def getData(self, index: int, chunk_size=1):
+    def getData(self, index: int):
         """
-        Return the data from a specific index. The data returned is a 1d array to
-        match the output from VStack
-
+        Return the data from a specific index. It need to add a dimension to match returned data from _Wave
         Parameters
         ----------
         index : :obj:`int`
             Index of the first shot of the chunck
-        chunk_size : :obj:`int`
-            Size of the chunk. Delimits how many shots will have data returned
         """
         with segyio.open(self.segyfile, "r", ignore_geometry=True) as f:
             retrieved_shot = []
 
-            for nshot in range(index, index + chunk_size):
-                position = self.table[nshot]['Trace_Position']
-                traces_in_shot = self.table[nshot]['Num_Traces']
-                shot_traces = f.trace[position:position + traces_in_shot]
+            position = self.table[index]['Trace_Position']
+            traces_in_shot = self.table[index]['Num_Traces']
+            shot_traces = f.trace[position:position + traces_in_shot]
 
-                for trace in shot_traces:
-                    retrieved_shot.append(trace)
-        # concatena todos os dados em um np.array 1D
-        return np.concatenate(retrieved_shot)
-
-    def _getOperatorChunk(self, shape, origin, spacing, vp0, nbl, space_order, t0, tn, src_type, f0, dtype, chunk, src0_idx, nshots):
-        """
-        Create a list of VStacks, where each VStack contains a specific number of AcousticWave2D operators.
-
-        Parameters
-        ----------
-        shape : tuple
-            Shape of the computational grid.
-        origin : tuple
-            Origin of the computational grid.
-        spacing : tuple
-            Spacing of the computational grid.
-        vp0 : ndarray
-            Velocity model.
-        nbl : int
-            Number of boundary layers.
-        space_order : int
-            Space order of the finite-difference scheme.
-        t0 : float
-            Start time of the simulation.
-        tn : float
-            End time of the simulation.
-        src_type : str
-            Type of the source (e.g., 'Ricker').
-        f0 : float
-            Peak frequency of the source.
-        dtype : type
-            Data type of the simulation (e.g., np.float32).
-        chunk : int
-            Number of shots/operators each VStack will encompass.
-        src0_idx : int
-            Starting index of the shots.
-        nshots : int or None
-            Total number of shots to process. If None, all shots are processed.
-
-        Returns
-        -------
-        Aops : list of VStack
-            A list of VStacks, where each VStack contains a specific number of AcousticWave2D operators.
-        """
-        nsrc = nshots if nshots else self.nsrc
-
-        # Ajusta o limite superior para não ultrapassar o número total de fontes
-        if src0_idx >= self.nsrc :
-            raise ValueError(f"src0_idx ({src0_idx}) is out of bounds. It must be less than the total number of sources ({self.nsrc}).")
-
-        # Limit the loop to always stay within the maximum number of shots. This ensures that
-        # if the starting index is too high and the sum with the desired number of sources exceeds
-        # the maximum index in the file, the loop remains within bounds.
-        end_idx = min(src0_idx + nsrc, self.nsrc)
-
-        Aops = []
-        ops = []
-        for ii, isrc in enumerate(range(src0_idx, end_idx)):
-            sx, sz = self.getSourceCoords(isrc)
-            rx, rz = self.getReceiverCoords(isrc)
-
-            Aop = AcousticWave2D(shape=shape, origin=origin, spacing=spacing, vp=vp0, nbl=nbl, space_order=space_order,
-                                 src_x=sx, src_z=sz, rec_x=rx, rec_z=rz, t0=t0, tn=tn, src_type=src_type, f0=f0, dtype=dtype,
-                                 op_name="fwd")
-            ops.append(Aop)
-
-            # If it has created the _wave operator chunk times, it creates Vstacks and reinitializes the ops list
-            if (ii + 1) % chunk == 0:
-                Aops.append(VStack(ops))
-                ops = []
-        # Append any remaining operators to Aops
-        if ops:
-            Aops.append(VStack(ops))
-        return Aops
-
-    def _getOperatorUnique(self, shape, origin, spacing, vp0, nbl, space_order, t0, tn, src_type, f0, dtype, src0_idx, nshots):
-        nsrc = nshots if nshots else self.nsrc
-
-        # Ajusta o limite superior para não ultrapassar o número total de fontes
-        end_idx = min(src0_idx + nsrc, self.nsrc)
-
-        Aops = []
-        for isrc in range(src0_idx, end_idx):
-            sx, sz = self.getSourceCoords(isrc)
-            rx, rz = self.getReceiverCoords(isrc)
-            Aop = AcousticWave2D(shape=shape, origin=origin, spacing=spacing, vp=vp0, nbl=nbl, space_order=space_order,
-                                 src_x=sx, src_z=sz, rec_x=rx, rec_z=rz, t0=t0, tn=tn, src_type=src_type, f0=f0, dtype=dtype,
-                                 op_name="fwd")
-            Aops.append(Aop)
-        return VStack(Aops)
-
-    def getOperator(self, shape, origin, spacing, vp0, nbl, space_order, t0, src_type, f0, dtype, chunk=None, src0_idx=1, nshots=None):
-        """
-        The idea is that each operator is directly related to a shot present in the SEGY file, so we will not have
-        problems with the dimensions of the output data. The basic idea would be a VStack containing
-        nshots operators. However, the size of the data can exceed the computer's memory capacity. To work around
-        this problem, it was defined that a list of VStacks would be created where each VStack would contain a specific
-        number of operators (chunk).
-
-        Thus, chunk defines how many shots/AcousticWave2D operators each VStack will encompass.
-        """
-        tn = self.getTn()
-
-        if chunk:
-            return self._getOperatorChunk(shape, origin, spacing, vp0, nbl, space_order, t0, tn, src_type, f0, dtype, chunk, src0_idx, nshots)
-        return self._getOperatorUnique(shape, origin, spacing, vp0, nbl, space_order, t0, tn, src_type, f0, dtype, src0_idx, nshots)
+            for trace in shot_traces:
+                retrieved_shot.append(trace)
+        return np.expand_dims(np.array(retrieved_shot), axis=0)
